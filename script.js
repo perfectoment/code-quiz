5a79ba5aea654652350229194bca1416733f5485
Lee Marvin <ryenem8@gmail.com>
2020-09-23 15:17:05 -0700

    creating function for checking answers

--- script.js
+++ script.js
@@ -12,36 +12,49 @@ var button1 = document.getElementById("btn1");
 var button2 = document.getElementById("btn2");
 var button3 = document.getElementById("btn3");
 var button4 = document.getElementById("btn4");
-// var time = document.getElementById("time");
-// var mainEl = document.getElementById("main");
-var secondsLeft = 0;
-var correctCounter ={}
+var secondsLeft = 60;
+var correctCounter = 0;
 
 let questions = [
     {
         title: "What is the HTML tag under which one can write the JavaScript code?",
-        choices: ["Header", "Body","Style", "Script"],
+        choices: ["Header","Body","Style","Script"],
         answer: "Script"
     },
     {
         title: "Choose the correct JavaScript syntax to change the content of the following HTML code.",
-        choices: ["document.getElement(“geek”).innerHTML", "document.getElementById(“geek”).innerHTML", "document.getId(“”)", "document.getElementById(“geek”).innerHTML"],
+        choices: ["document.getElement(“geek”).innerHTML","document.getElementById(“geek”).innerHTML","document.getId(“”)","document.getElementById(“geek”).innerHTML"],
         answer: "document.getElementById(“geek”).innerHTML",
     },
     {
         title: "What is the correct syntax for referring to an external script called “script.js”?",
-        choices: ["<script src=”geek.js”>", "<script href=”geek.js”>", "<script ref=”geek.js”>", "<script name=”geek.js”>"],
-        answer: "<script src=”geek.js”>",
+        choices: ["<script src=”script.js”>","<script href=”script.js”>", "<script ref=”script.js”>", "<script name=”script.js”>"],
+        answer: "<script src=”script.js”>",
     },
     {
         title: "Which of the following is not a reserved word in JavaScript?",
-        choices: ["interface", "throws", "program", "short"],
+        choices: ["interface","throws","program","short"],
         answer: "program",
     },
 ]
 
 /*EVETN LISTENERS */
+function questionChecker (event) {
+    console.log("Tripped!")
+    var textPuller = event.target.textContent;
+    if(textPuller === questions[currentQuestion].answer){
+        currentQuestion++;
+        correctCounter++;
+        showCurrentQuestion()
+        console.log(correctCounter);
+    }
+    else{
+         secondsLeft = (secondsLeft - 10);
+         currentQuestion++;
+         showCurrentQuestion();
+    }
 
+}
 
 // strtButton.addEventListener("click", function() {
 //     startContainer.setAttribute("class", "hide");
@@ -49,49 +62,34 @@ let questions = [
 //     questionsContainer.setAttribute("class", "card text-center"); 
 // }
 
-let question = questions[currentQuestion];
+// let question = questions[currentQuestion];
 // )
-button1.addEventListener("click", function () {
-    console.log("Tripped!")
-    if(question.choices[0] = question.answer[0]){
-        currentQuestion++
-        showCurrentQuestion()
-    }
-    else{
-         secondsLeft = -10
-         currentQuestion++
-         showCurrentQuestion()
-    }
-})
-button2.addEventListener("click", function () {
-    currentQuestion++
-    showCurrentQuestion()
-})
-
-button3.addEventListener("click", function () {
-    currentQuestion++
-    showCurrentQuestion()
-})
-
-button4.addEventListener("click", function () {
-    currentQuestion++
-    showCurrentQuestion()
-})
+button1.addEventListener("click", questionChecker);
+button2.addEventListener("click", questionChecker);
+button3.addEventListener("click", questionChecker);
+button4.addEventListener("click", questionChecker);
 
 
 /*FUNCTIONS */
 function showCurrentQuestion() {
-    let question = questions[currentQuestion];
-    questionsDisplay.textContent = question.title;
-    button1.textContent = question.choices[0];
-    button2.textContent = question.choices[1];
-    button3.textContent = question.choices[2];
-    button4.textContent = question.choices[3];
+    //   if(question.choices > 4)  {
+        let question = questions[currentQuestion];
+        questionsDisplay.textContent = question.title;
+        button1.textContent = question.choices[0];
+        button2.textContent = question.choices[1];
+        button3.textContent = question.choices[2];
+        button4.textContent = question.choices[3];
     
     
-     
+      }
+    //   else{        
+    //     startContainer.setAttribute("class", "hide");
+    //     initalsContainer.setAttribute("class", "card text-center position-absolute");
+    //     questionsContainer.setAttribute("class", "hide"); 
+
+    //   }
     console.log(question)
-}
+
 // }
        
       
@@ -106,9 +104,9 @@ function start() {
     }
     )
     strtButton.addEventListener("click", function() {
-    var secondsLeft = 60;
+    secondsLeft = 60;
     var time = document.getElementById("time");
-    var mainEl = document.getElementById("main");
+    // var mainEl = document.getElementById("main");
     
     // strtButton.addEventListener("click", start); {
 
